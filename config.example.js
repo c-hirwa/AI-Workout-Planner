00acc70caf5cc93fdf4bf0c7f53ab92b1151047a
c-hirwa <c.hirwa@alustudent.com>
2025-04-28 16:07:28 +0200

    Update config.example.js

---
+++ config.example.js
@@ -0,0 +1,7 @@
+// config.example.js - Commit this file as an example
+const config = {
+    API_KEY: 'YOUR_API_KEY_HERE'
+};
+
+export default config;
+// If using CommonJS: module.exports = config;
